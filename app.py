cf7d7bc53ed0142a29836dfdc2825e586bdcfbb5
Schirlei Alves <89231147+Schirlei@users.noreply.github.com>
2021-12-20 22:08:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+import base64
+import gspread
+import json
+import os
 
-app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Os gastos dos deputados catarinenses com diárias.</p>"
+spreadsheet_id = os.environ["GOOGLE_SHEET_ID"]
+conteudo_codificado = os.environ["GOOGLE_SHEETS_CREDENTIALS"]
+conteudo = base64.b64decode(conteudo_codificado)
+credentials = json.loads(conteudo)
+
+service_account = gspread.service_account_from_dict(credentials) #Faz a autenticação
+spreadsheet = service_account.open_by_key(spreadsheet_id) #Conecta à planilha
+worksheet = spreadsheet.worksheet("Planilha1") #Escolhe a aba
