2b9f5308fc08adfb3761f4ab0e77421c741b1fd3
Schirlei Alves <89231147+Schirlei@users.noreply.github.com>
2022-01-06 15:50:20 -0300

    Update diarias_sc.py

--- diarias_sc.py
+++ diarias_sc.py
@@ -1,14 +1,15 @@
 import pandas as pd
 from datetime import datetime, timedelta
 def raspar_alesc_escrever_spreadsheet(worksheet):
-    df = pd.read_csv('https://transparencia.alesc.sc.gov.br/diarias_csv.php?ano=2021',
+    df = pd.read_csv('https://transparencia.alesc.sc.gov.br/diarias_csv.php?ano=2022',
                      sep=';',
                      encoding='iso 8859-1')
     dados = df.to_dict('records')
     ontem = datetime.today() - timedelta(days=1)
     for dado in dados:
-        data = datetime.strptime(dado['Data'], '%d/%m/%Y')
+        print(list(dado.values())
+        #data = datetime.strptime(dado['Data'], '%d/%m/%Y')
         #se a data for maior do que ontem, acrescentar à planilha
-        if data > ontem:
+        #if data > ontem:
             #acresentar à planilha
-            worksheet.append_row(list(dado.values()))
+            #worksheet.append_row(list(dado.values()))
