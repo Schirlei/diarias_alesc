cb26e880eb0784a63a9f21839520baa9a3420ccb
Schirlei Alves <89231147+Schirlei@users.noreply.github.com>
2022-01-11 20:16:07 -0300

    Update atualizar.py

--- atualizar.py
+++ atualizar.py
@@ -16,16 +16,20 @@ spreadsheet = service_account.open_by_key(spreadsheet_id) #Conecta à planilha
 worksheet = spreadsheet.worksheet("Página1") #Escolhe a aba
 
 def raspar_alesc_escrever_spreadsheet(worksheet):
-    df = pd.read_csv('https://transparencia.alesc.sc.gov.br/diarias_csv.php?ano=2022',
+    df = pd.read_csv('https://transparencia.alesc.sc.gov.br/diarias_csv.php?ano=2021'),
                      sep=';',
                      encoding='iso 8859-1')
     dados = df.to_dict('records')
     ontem = datetime.today() - timedelta(days=1)
     for dado in dados:
-        data = datetime.strptime(dado['Data'], '%d/%m/%Y')
+        exists = worksheet.findall(dado['Relatório'])
+        if not exists:
+            worksheet.append_row(list(dado.values())
+                                 
+        #data = datetime.strptime(dado['Data'], '%d/%m/%Y')
         #se a data for maior do que ontem, acrescentar à planilha
-        if data > ontem:
-            worksheet.append_row(list(dado.values()))
+        #if data > ontem:
+            #worksheet.append_row(list(dado.values()))
             
 #torna .py "executável"
 if __name__ == '__main__':
