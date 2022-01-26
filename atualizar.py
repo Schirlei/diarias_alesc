b912f0d5a2e00af99586e23868dffc0fa1f282c1
Schirlei Alves <89231147+Schirlei@users.noreply.github.com>
2022-01-26 11:07:07 -0300

    Update atualizar.py

--- atualizar.py
+++ atualizar.py
@@ -18,7 +18,7 @@ worksheet = spreadsheet.worksheet("Página1") #Escolhe a aba
 valores_atuais = worksheet.col_values(7)
 
 def raspar_alesc_escrever_spreadsheet(worksheet):
-    df = pd.read_csv('https://transparencia.alesc.sc.gov.br/diarias_csv.php?ano=2022',
+    df = pd.read_csv('https://transparencia.alesc.sc.gov.br/diarias_csv.php?ano=2021',
                      sep=';',
                      encoding='iso 8859-1')
     dados = df.to_dict('records')
